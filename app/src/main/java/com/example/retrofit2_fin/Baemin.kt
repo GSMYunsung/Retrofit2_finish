f963d1bf5b8558f8ce0027cd4e30ebf4e4b2cad9
yunsung <12345ggh81@gmail.com>
2021-07-05 21:17:19 +0900

    [CREATE] Retrofit2_dataclass

---
+++ app/src/main/java/com/example/retrofit2_fin/Baemin.kt
@@ -0,0 +1,14 @@
+package com.example.retrofit2_fin
+
+data class baeminData(
+    val data: Data
+    )
+
+data class Data(
+    val content: ArrayList<Content>
+    )
+
+data class Content(
+    val title: String,
+    val created: String
+    )
