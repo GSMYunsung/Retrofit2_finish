4d0d67882d31b6e22f73f6521a73d795baa3ef78
yunsung <12345ggh81@gmail.com>
2021-07-06 12:27:10 +0900

    [UPDATE] DataClass

--- app/src/main/java/com/example/retrofit2_fin/Baemin.kt
+++ app/src/main/java/com/example/retrofit2_fin/Baemin.kt
@@ -1,14 +1,11 @@
 package com.example.retrofit2_fin
 
-data class baeminData(
-    val data: Data
-    )
+data class baeminData(val data: Data)
 
-data class Data(
-    val content: ArrayList<Content>
-    )
+data class Data(val content: ArrayList<Content>)
 
 data class Content(
     val title: String,
-    val created: String
+    val created: String,
+    val content: String
     )
